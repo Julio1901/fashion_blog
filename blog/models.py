26b8074b30d00802d9c7dc2d7b9f756779022f0b
Julio Cesar <js.cesar42@gmail.com>
2021-01-11 23:15:10 -0300

    Finished project

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from django.urls import reverse
 
 class Post(models.Model):
     
@@ -21,6 +22,14 @@ class Post(models.Model):
                     
                     #Note que esse não tem o add igual acima
     updated = models.DateTimeField(auto_now=True)
+    
+
+    #Essa meta está fazendo os posts serem exibidos do mais recente para o mais antigo
+    class Meta:
+        ordering = ('-created',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'slug': self.slug})
